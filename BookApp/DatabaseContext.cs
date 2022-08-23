2e82f26cb750872321f7f6d32ec00289c5f142a6
OWNER <OWNER@15AC>
2022-08-23 11:42:05 +0530

    Add project files.

---
+++ BookApp/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using BookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApp
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<BookApp.Models.UserAccounts>? UserAccounts { get; set; }
+      
+
+        public DbSet<BookApp.Models.CartItems>? CartItems { get; set; }
+
+        public DbSet<BookApp.Models.Billing>? Billing { get; set; }
+
+        public DbSet<BookApp.Models.Transaction>? Transaction { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=BookApp;Integrated Security=true");
+        }
+    }
+}
